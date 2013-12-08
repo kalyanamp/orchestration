932a551b3a9018373bd12afad0ede1b60c9fb188
mbadran <mebadran@gmail.com>
2013-12-09 10:46:49 +1100

    restructure, updates, fixes

--- openam/roles/common/templates/setenv.sh
+++ openam/roles/common/templates/setenv.sh
@@ -1,7 +1,7 @@
 # {{ ansible_managed }}
 
-{% if openam_java_opts %}
-set CATALINA_OPTS={{ java_opts }}
+{% if openam_java_opts is defined %}
+set CATALINA_OPTS={{ openam_java_opts }}
 {% else %}
 set CATALINA_OPTS=-Xmx1024m -XX:MaxPermSize=256m
 {% endif %}
