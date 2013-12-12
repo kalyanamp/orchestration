a3c44a6118ae7ea7ba5d216e33ca6d09c26b2228
mbadran <mebadran@gmail.com>
2013-12-12 21:47:33 +1100

    fixes

--- openam/install.sh
+++ openam/install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ansible-playbook --verbose --inventory-file stage --user root install.yml --extra-vars '{"hosts":"fresh1.example.com", "hostname":"oam1.example.com", "cookie_domain":".example.com", "deployment_uri":"am", "root_suffix":"o=openam,dc=example,dc=com", "admin_password":"helloworld", "policy_agent_password":"heyworld", "config_store_password":"helloworld", "install_admin_tools":"yes"}'
+ansible-playbook --verbose --inventory-file stage --user root install.yml --extra-vars '{"hosts":"dev1.example.com", "hostname":"oam1.example.com", "cookie_domain":".example.com", "deployment_uri":"am", "root_suffix":"o=openam,dc=example,dc=com", "admin_password":"helloworld", "policy_agent_password":"heyworld", "config_store_password":"helloworld", "install_admin_tools":"yes"}'
--- openam/uninstall.sh
+++ openam/uninstall.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ansible-playbook --verbose --inventory-file stage --user root uninstall.yml --extra-vars '{"hosts":"fresh1.example.com", "deployment_uri":"am"}'
+ansible-playbook --verbose --inventory-file stage --user root uninstall.yml --extra-vars '{"hosts":"dev1.example.com", "deployment_uri":"am"}'
